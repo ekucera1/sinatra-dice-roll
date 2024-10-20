cfd4aac49b15680cae7766b8f496065c9eaf5640
ekucera1 <ekucera@uchicago.edu>
2024-10-20 21:13:19 +0000

    changes

--- dice.rb
+++ dice.rb
@@ -21,11 +21,18 @@ get("/giraffe") do
 end
 
 get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
+  @rolls = []
+
+  2.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+  #first_die = rand(1..6)
+	#second_die = rand(1..6)
+  #sum = first_die + second_die
 	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	#@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
 	erb(:two_six)
 end
